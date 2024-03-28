7e1a502d24edc3ae26d808a55c78d9b93f67d327
Kostadin <kostadin@Kostadins-Mac-mini.local>
2024-03-28 22:14:50 +0200

    Initial code

---
+++ src/test/java/com/kmanchev/caching/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmanchev.caching.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
